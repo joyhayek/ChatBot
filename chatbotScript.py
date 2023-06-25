dcb567683f2178cf99e41ea45207378cd39f1014
joyhayek <joy.hayek@hotmail.com>
2023-06-25 14:54:31 +1000

    # Exception Handling and Saving 1. Saving data to pickle file 2. Saving model 3. Handling exceptions

--- chatbotScript.py
+++ chatbotScript.py
@@ -1,12 +1,14 @@
 # Start by importing modules that will be used to read in the data from the json file
 
 import nltk
+nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
-import numpy as np
+import numpy 
 import tflearn
 import tensorflow
 import json
 import random
+import pickle
 
 stemmer = LancasterStemmer()
 
@@ -14,70 +16,78 @@ stemmer = LancasterStemmer()
 with open("data.json") as json_data:
     data = json.load(json_data)
 
-# print(data)
-
-# Create lists to store the data in the json file
-words = []
-# This stores the tags
-labels = []
-# this stores the patterns
-docs_x = []
-# this stores the tag of each pattern
-docs_y = []
-
-# Loop through the data 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        # Tokenize each word in patterns and add the tokens to the words list
-        tokens = nltk.word_tokenize(pattern)
-        words.extend(tokens)
-        # Add the tokens to the docs_x list
-        docs_x.append(tokens)
-        # Add the tag of each pattern to the docs_y list
-        docs_y.append(intent["tag"])
-        
-    # Add the tags to the tags list if not in list aleady
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
-        
-# Stem all tokens
-words = [stemmer.stem(token.lower()) for token in words if token != "?"]
-# Store the words in a sorted list that doesn't contain any duplicates
-words = sorted(list(set(words)))
-# Sort the labels/tags
-labels = sorted(labels)
-
-# Perform one-hot-encoding on the words - neural networks only understand numbers and not words
-# We will store 0 if the word doesn't exist in the prompt, and 1 if it does
-training = []
-output = []
-
-out_empty = [0 for _ in range(len(labels))]
-
-for x, doc in enumerate(docs_x):
-    bag = []
-    # Stem each token in docs_x
-    stemmed_tokens = [stemmer.stem(d) for d in doc]
-    # Encode each stemmed word based on the pattern and append to bag
-    for word in words:
-        if word in stemmed_tokens:
-            bag.append(1)
-        else:
-            bag.append(0)
-
-    # Generate the encoded output lists
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+try:
+    # Load the data from the pickle file
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
     
-    training.append(bag)
-    output.append(output_row)
+except:
+    # Create lists to store the data in the json file
+    words = []
+    # This stores the tags
+    labels = []
+    # this stores the patterns
+    docs_x = []
+    # this stores the tag of each pattern
+    docs_y = []
+
+    # Loop through the data 
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            # Tokenize each word in patterns and add the tokens to the words list
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            # Add the tokens to the docs_x list
+            docs_x.append(wrds)
+            # Add the tag of each pattern to the docs_y list
+            docs_y.append(intent["tag"])
+            
+        # Add the tags to the tags list if not in list aleady
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+            
+    # Stem all tokens
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    # Store the words in a sorted list that doesn't contain any duplicates
+    words = sorted(list(set(words)))
+    # Sort the labels/tags
+    labels = sorted(labels)
+
+    # Perform one-hot-encoding on the words - neural networks only understand numbers and not words
+    # We will store 0 if the word doesn't exist in the prompt, and 1 if it does
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+        # Stem each token in docs_x
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+        # Encode each stemmed word based on the pattern and append to bag
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        # Generate the encoded output lists
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+        
+        training.append(bag)
+        output.append(output_row)
+        
+    # Turn the training and output lists into numpy arrays
+    training = numpy.array(training)
+    output = numpy.array(output)
     
-# Turn the training and output lists into numpy arrays
-training = np.array(training)
-output = np.array(output)
+    # Save all of this data in a pickle file
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
 # Creating the model
-tensorflow.reset_default_graph()
+tensorflow.compat.v1.reset_default_graph()
 
 net = tflearn.input_data(shape=[None, len(training[0])])
 net = tflearn.fully_connected(net, 8)
@@ -88,7 +98,12 @@ net = tflearn.regression(net)
 # Training the model
 model = tflearn.DNN(net)
 
-# Fit the model
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-# Save the model
-model.save("model.tflearn")
+try:
+    model.load("model.tflearn")
+except Exception:
+    # Fit the model
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    # Save the model
+    model.save("model.tflearn")
+
+

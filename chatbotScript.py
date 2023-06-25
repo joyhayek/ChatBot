72189c6016c28249b097b9e2c03f4bbfaf995d07
joyhayek <joy.hayek@hotmail.com>
2023-06-25 14:55:29 +1000

    Created a bag of words function to predict

--- chatbotScript.py
+++ chatbotScript.py
@@ -107,3 +107,16 @@ except Exception:
     model.save("model.tflearn")
 
 
+# Start making predictions
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+                
+    return numpy.array(bag)
